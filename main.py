fc9df5dca91743819957b4600a24d1cf41482fde
Marco Pavía <142189176+Marco-Pavia@users.noreply.github.com>
2024-09-27 17:20:59 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from formularios.form_login import FormLoginDesigner
 from formularios.form_maestro_design import FormularioMaestroDesign
 
-app = FormularioMaestroDesign()
-app.mainloop()
+def main():
+    # Ejecutar el formulario de login
+    login = FormLoginDesigner()
+    login.ventana.mainloop()  # Muestra la ventana de login
+
+    # Solo si el login es exitoso, se ejecuta FormularioMaestroDesign
+    if login.es_login_exitoso():
+        maestro = FormularioMaestroDesign()  # Inicia el formulario maestro
+        maestro.mainloop()  # Ciclo principal de la aplicación principal
+
+if __name__ == "__main__":
+    main()
